83c627ca73ccdb7bee6060698802f498ee8eca89
Your Name <youremail@yourdomain.com>
2024-05-16 19:47:44 +0300

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Matrix from './Matrix.svelte';
+	// import { Dropdown, DropdownItem, DropdownDivider, DropdownHeader } from 'flowbite-svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<!-- <Datepicker datepickerFormat="dd/mm/yyyy" datepickerTitle="Дата народження" $$restProps="dasd" /> -->
+	<Matrix />
+</section>
