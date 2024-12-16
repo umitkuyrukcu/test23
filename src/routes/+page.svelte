6a5c980b619b7298cdafba5b734411ba595b150d
Your Name <youremail@yourdomain.com>
2024-12-16 09:46:17 +0300

    header güncellendi

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,8 @@
 	<title>Kader Matrisi Hesaplama</title>
 	<meta name="keywords" content="Kader Matrisi Hesaplama, Kader Matrisi Hesapla, Ücretsiz Kader Matrisi Hesaplayıcı">	
 <meta name="description" content="Kader Matrisi Hesaplayıcı, Doğum Tarihinden Kader Matrisi Hesaplama, Kader Metrisi Uyumluluk Hesapla">
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6506575288548257"
+     crossorigin="anonymous"></script>
 </svelte:head>
 
 <section>
