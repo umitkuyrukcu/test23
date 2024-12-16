7cf5fbf407202d9d9a14686848a9e8c4a89ed5e5
Your Name <youremail@yourdomain.com>
2024-12-16 09:49:51 +0300

    head google

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <svelte:head>
-	<meta charset="utf-8" />
-	<title>Kader Matrisi Hesaplama</title>
-	<meta name="keywords" content="Kader Matrisi Hesaplama, Kader Matrisi Hesapla, Ücretsiz Kader Matrisi Hesaplayıcı">	
-<meta name="description" content="Kader Matrisi Hesaplayıcı, Doğum Tarihinden Kader Matrisi Hesaplama, Kader Metrisi Uyumluluk Hesapla">
+<meta charset="utf-8" />
+<title>Kader Matrisi Hesaplama | Kader Matrix</title>
+<meta name="keywords" content="Kader Matrisi Hesaplama, Kader Matrisi Hesapla, Ücretsiz Kader Matrisi Hesaplayıcı">	
+<meta name="description" content="Kader Matrisi Hesaplayıcı, Doğum Tarihinden Kader Matrisi Hesaplama, Kader Matrisi Uyumluluk Hesapla">
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6506575288548257"
      crossorigin="anonymous"></script>
 </svelte:head>
