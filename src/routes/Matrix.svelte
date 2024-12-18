e0bd49e09576f85c2f860f523b144005492b4886
Your Name <youremail@yourdomain.com>
2024-12-18 14:48:49 +0300

    coffe

--- src/routes/Matrix.svelte
+++ src/routes/Matrix.svelte
@@ -1,3 +1,5 @@
+<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="kadermatrisi" data-color="#FFDD00" data-emoji="☕" data-font="Çerez" data-text="Bana Bir Kahve Ismarla" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
+
 <script>
 	export let a = 'a';
 	export let a1 = 'a1';
